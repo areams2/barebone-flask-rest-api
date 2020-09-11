00d61e763f820ada7d442d1ab89c6b97282935b1
Amanda Reams <amandareams@amandas-mbp.lan>
2020-09-11 19:17:36 -0400

    updates.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# barebone-flask-rest-api
-
-Check out the full tutorial: [https://camillovisini.com/barebone-flask-rest-api-on-aws-elastic-beanstalk/](https://camillovisini.com/barebone-flask-rest-api-on-aws-elastic-beanstalk/)
+# create virtual environment and activate it
+python3 -m venv my_virtual_environment
+source my_virtual_environment/bin/activate
+# within virtual environment, install dependencies:
+pip3 install -r requirements.txt
+#run api locally
+python3 app.py --debug (can remove flag if desired.)
--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('mysql://admin:qk9U2jswyDT3KNDY@sb-koru-flask.cqic4eehbwsk.us-east-1.rds.amazonaws.com/koru_test', convert_unicode=True)
+engine = create_engine('mysql+pymysql://admin:qk9U2jswyDT3KNDY@sb-koru-flask.cqic4eehbwsk.us-east-1.rds.amazonaws.com/koru_test', convert_unicode=True)
 metadata = MetaData()
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
